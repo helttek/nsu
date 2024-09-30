fd14fe09d334988c3fc722c66f8c44d787799482
Anton <a.stolyarov@g.nsu.ru>
2024-09-30 16:08:13 +0700

    updating module convergence plots

--- module_interpolation_convergence/NewtonPolynomial.cpp
+++ module_interpolation_convergence/NewtonPolynomial.cpp
@@ -1,19 +1,20 @@
 #include "NewtonPolynomial.hpp"
 #include <fstream>
+#include <iostream>
 
-NewtonPolynomial::NewtonPolynomial(const std::vector<double> nodes) : nodes(nodes)
-{
-    for (size_t i = 0; i < nodes.size(); i++)
-    {
-        /* code */
+NewtonPolynomial::NewtonPolynomial(const std::vector<double> nodes)
+    : nodes(nodes) {}
+
+NewtonPolynomial::~NewtonPolynomial() {}
+
+void NewtonPolynomial::CalcAndDump(std::string outFile) {
+  std::ofstream out(outFile);
+  if (!out.is_open()) {
+    std::cerr << "Error: couldn't open output file\n";
+    return;
+  }
+  for (size_t i = 0; i < nodes.size(); ++i) {
+    for (size_t j = 0; j < i; ++j) {
     }
-}
-
-NewtonPolynomial::~NewtonPolynomial()
-{
-}
-
-void NewtonPolynomial::CalcAndDump(std::string outFile)
-{
-    std::ofstream out(outFile);
+  }
 }
--- module_interpolation_convergence/main.cpp
+++ module_interpolation_convergence/main.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
-#include <fstream>
 #include "NewtonPolynomial.hpp"
+#include <cstddef>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main()
-{
-    std::vector<double> nodes(11);
-    NewtonPolynomial newPol(nodes);
-    newPol.CalcAndDump("output.txt");
-    return EXIT_SUCCESS;
+void ReadInput(std::string in, std::vector<double> &nodes) {
+  std::ifstream fin(in);
+  size_t i = 0;
+  while (i < nodes.size() && !fin.eof()) {
+    fin >> nodes[i];
+  }
+}
+
+int main() {
+  std::vector<double> nodes(11);
+  ReadInput("input", nodes);
+  NewtonPolynomial newPol(nodes);
+  newPol.CalcAndDump("output.txt");
+  return EXIT_SUCCESS;
 }
