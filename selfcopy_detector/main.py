081195080eaa586deee529e9f808300c72262723
anton stolyarov <a.stolyarov@g.nsu.ru>
2024-10-16 22:41:33 +0700

    update

--- selfcopy_detector/main.py
+++ selfcopy_detector/main.py
@@ -1,10 +1,5 @@
 from selfCopyDetector import SelfCopyDetector
 import sys
-import socket
 
-if (len(sys.argv) == 1):
-    print("No args provided")
-    sys.exit(0)
-
-scd = SelfCopyDetector(sys.argv[1])
-scd.Start()
+scd = SelfCopyDetector(sys.argv)
+scd.start()
--- selfcopy_detector/selfCopyDetector.py
+++ selfcopy_detector/selfCopyDetector.py
@@ -2,30 +2,41 @@ import socket
 import sys
 import ipaddress
 import struct
-from time import sleep
-
 
 class SelfCopyDetector:
-    PORT = 50000
+    DEFAULT_PORT = 50000
+    DEFAULT_IP_ADDRESS = '224.0.0.1'
 
-    def __init__(self, address):
-        self.ipaddr = address
+    def __init__(self, argv):
+        if len(argv) == 1:
+            self.ipaddress = self.DEFAULT_IP_ADDRESS
+            self.port = self.DEFAULT_PORT
+        if len(argv) == 2:
+            self.ipaddr = argv[1]
+            self.port = self.DEFAULT_PORT
+        if len(argv) == 3:
+            self.ipaddr = argv[1]
+            self.port = argv[2]
+        if not self.__createSocket():
+            raise Exception("Failed to create socket")
 
-    def Start(self):
-        self.__CheckIPAddress()
+    def start(self):
 
-    def __CheckIPAddress(self):
+
+    def __createSocket(self):
         try:
             ip = ipaddress.ip_address(self.ipaddr)
         except ValueError:
             print("invalid IP address: ", self.ipaddr)
-            sys.exit(0)
-        if (ip.version == 4):
-            self.__CreateSocketIPv4()
+            return False
+        if ip.version == 4:
+            self.__createSocketIPv4()
+            return True
         else:
-            self.__CreateSocketIPv6()
+            self.__createSocketIPv6()
+            return True
 
-    def __CreateSocketIPv4(self):
+    def __createSocketIPv4(self):
         print("ipv4")
         try:
             self.sock = socket.socket(
@@ -33,12 +44,12 @@ class SelfCopyDetector:
             ttl = struct.pack('b', 1)
             self.sock.setsockopt(
                 socket.IPPROTO_IPV4, socket.IP_MULTICAST_TTL, ttl)
-            self.sock.bind((self.ipaddr, self.PORT))
+            self.sock.bind((self.ipaddr, self.port))
         except OSError as e:
             print("couldn't create socket: ", e)
             sys.exit(0)
 
-    def __CreateSocketIPv6(self):
+    def __createSocketIPv6(self):
         print("ipv6")
         try:
             self.sock = socket.socket(
