5ae8a03843f737a3ba1461dc80dfbd00b8ab51b5
anton <a.stolyarov@g.nsu.ru>
2025-08-17 15:42:25 +0700

    removing duplicate

--- networks/2_sem/selfcopy_detector/main.py
+++
@@ -1,9 +0,0 @@
-from self_copy_detector import self_copy_detector
-import sys
-
-try:
-    scd = self_copy_detector(sys.argv)
-    scd.start()
-except RuntimeError as e:
-    print(e)
-    sys.exit(1)
--- networks/2_sem/snake_game/src/main/java/fit/snake_game/model/exceptions/WrongArgException.java
+++
@@ -1,7 +0,0 @@
-package fit.snake_game.model.exceptions;
-
-public class WrongArgException extends RuntimeException {
-    public WrongArgException(String message) {
-        super(message);
-    }
-}
