ca5fbdae7c979fcd484e5151573c1162b94a83c5
anton stolyarov <a.stolyarov@g.nsu.ru>
2026-02-27 20:06:10 +0700

    set up docker and gradle to work on the project

---
+++ ris/CrackHash/manager-service/src/main/java/org/crackhash/Manager.java
@@ -0,0 +1,10 @@
+package org.crackhash;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Manager {
+    public static void main(String[] args) {
+        System.out.println("Hello worker");
+    }
+}
---
+++ ris/CrackHash/worker-service/src/main/java/org/crackhash/Worker.java
@@ -0,0 +1,10 @@
+package org.crackhash;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Worker {
+    public static void main(String[] args) {
+        System.out.println("Hello manager");
+    }
+}
