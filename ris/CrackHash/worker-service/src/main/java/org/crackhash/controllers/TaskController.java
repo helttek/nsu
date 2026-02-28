775623dc4f434f6ea3149f16a1c5e6e5dcdaa944
anton stolyarov <a.stolyarov@g.nsu.ru>
2026-02-28 22:30:15 +0700

    added RestClient dependency

--- ris/CrackHash/worker-service/src/main/java/org/crackhash/controllers/TaskController.java
+++ ris/CrackHash/worker-service/src/main/java/org/crackhash/controllers/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
+
     @PostMapping("/internal/api/worker/hash/crack/task")
     public String getTask() {
         return "Task";
