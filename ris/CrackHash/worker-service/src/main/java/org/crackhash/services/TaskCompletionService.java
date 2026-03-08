82e269b6175407ae2935bcae5899369f999780bb
anton stolyarov <a.stolyarov@g.nsu.ru>
2026-03-08 13:41:34 +0700

    upd

--- ris/CrackHash/worker-service/src/main/java/org/crackhash/services/TaskCompletionService.java
+++ ris/CrackHash/worker-service/src/main/java/org/crackhash/services/TaskCompletionService.java
@@ -30,31 +30,35 @@ public class TaskCompletionService {
         range.calculate(dto.getPartNumber(), dto.getPartCount(), maxLength, alphabet.size());
 
         long rangeCount = range.start;
-        log.info("starting length: {}", range.startingLength);
-        log.info("ending length: {}", range.endingLength);
+        log.info("starting length: {}", range.startingCombinationLength);
+        log.info("ending length: {}", range.endingHashLength);
         log.info("start: {}", range.start);
         log.info("end: {}", range.end);
-        for (int hashLength = range.startingLength; hashLength <= range.endingLength; hashLength++) {
+        for (int hashLength = range.startingCombinationLength; hashLength <= range.endingHashLength; hashLength++) {
             log.info("hash length: {}", hashLength);
             Iterable<List<String>> generator = Generator.permutation(alphabet)
                     .withRepetitions(hashLength);
 
-            //TODO: fix, when different workers share a generator with the same hashLength
+            long i = 0;
             for (List<String> combination : generator) {
-//                log.info("range count: {}", rangeCount);
-                if (rangeCount > range.end) {
-                    break;
-                }
-                rangeCount++;
+                //TODO: introduce 2-element array and checks for starting and ending combination length
+                if (range.generatorIterationStart <= i && i <= range.generatorIterationEnd) {
+                    //                log.info("range count: {}", rangeCount);
+                    if (rangeCount > range.end) {
+                        break;
+                    }
+                    rangeCount++;
 
-                String candidate = String.join("", combination);
+                    String candidate = String.join("", combination);
 //                log.info("Processing word: {}", candidate);
 
-                if (DigestUtils.md5Hex(candidate).equals(hash)) {
-                    log.info("Range: {}", rangeCount);
-                    log.info("Found word with a matching hash: {}", candidate);
-                    return;
+                    if (DigestUtils.md5Hex(candidate).equals(hash)) {
+                        log.info("Range: {}", rangeCount);
+                        log.info("Found word with a matching hash: {}", candidate);
+                        return;
+                    }
                 }
+                ++i;
             }
         }
 
@@ -64,13 +68,45 @@ public class TaskCompletionService {
     @Component
     @Getter
     private static class Range {
+        /**
+         * Number of the first combination the worker has to check.
+         *
+         */
         private long start;
+        /**
+         * Number of the last combination the worker has to check.
+         *
+         */
         private long end;
+        /**
+         * Number of combinations, the worker has to check.
+         *
+         */
         private long count;
-        private int startingLength;
-        private int endingLength;
-        private long iterationStart;
-        private long iterationEnd;
+        /**
+         * Needed to determine from which combination length to start.
+         *
+         */
+        private int startingCombinationLength;
+        /**
+         * Needed to determine on which combination length to end.
+         *
+         */
+        private int endingHashLength;
+        /**
+         * Starting point for iterations in a generator with a certain hash length.
+         * Needed to determine, which combinations the current worker will be checking,
+         * if multiple workers happen to be given a generator with the same hash length.
+         *
+         */
+        private long generatorIterationStart;
+        /**
+         * Ending point for iterations in a generator with a certain hash length.
+         * Needed to determine, which combinations the current worker will be checking,
+         * if multiple workers happen to be given a generator with the same hash length.
+         *
+         */
+        private long generatorIterationEnd;
 
         private void calculate(int workerNumber, int workerCount, int hashMaxLength, int alphabetSize) {
             long count = 0;
@@ -81,9 +117,15 @@ public class TaskCompletionService {
             end = Math.min(start + count / workerCount, count);
             this.count = end - start;
 
+            calculateStartingLength(hashMaxLength, alphabetSize);
+            calculateEndingLength(hashMaxLength, alphabetSize);
+            calculateIterationBounds(alphabetSize);
+        }
+
+        private void calculateStartingLength(int hashMaxLength, int alphabetSize) {
             long t = 0;
             if (t >= start) {
-                startingLength = 1;
+                startingCombinationLength = 1;
             } else {
                 for (int hashLength = 1; hashLength <= hashMaxLength; hashLength++) {
                     t += (long) Math.pow(alphabetSize, hashLength);
@@ -91,28 +133,38 @@ public class TaskCompletionService {
                     log.info("t: {}", t);
                     log.info("start: {}", start);
                     if (t >= start) {
-                        startingLength = hashLength;
-                        iterationStart = start - 1;
+                        startingCombinationLength = hashLength;
+                        generatorIterationStart = start - 1;
                         break;
                     }
                 }
             }
-            t = 0;
-            endingLength = 0;
+        }
+
+        private void calculateEndingLength(int hashMaxLength, int alphabetSize) {
+            long t = 0;
+            endingHashLength = 0;
             for (int hashLength = 1; hashLength <= hashMaxLength; hashLength++) {
                 t += (long) Math.pow(alphabetSize, hashLength);
                 log.info("HAHHHHHAHAHHAHH");
                 log.info("t: {}", t);
                 log.info("end: {}", end);
                 if (t >= end) {
-                    endingLength = Math.max(hashLength, 1);
+                    endingHashLength = Math.max(hashLength, 1);
                     break;
                 }
             }
         }
 
-        private end calculateIterationBounds() {
-
+        private void calculateIterationBounds(int alphabetSize) {
+            long t = 0;
+            for (int combinationLength = 1; combinationLength <= startingCombinationLength - 1; combinationLength++) {
+                t += (long) Math.pow(alphabetSize, combinationLength);
+            }
+            generatorIterationStart = Math.max(start - (long) Math.pow(alphabetSize, startingCombinationLength - 1), 0);
+            generatorIterationEnd = end - (long) Math.pow(alphabetSize, endingHashLength - 1);
+            log.info("Iteration start: {}", generatorIterationStart);
+            log.info("Iteration end: {}", generatorIterationEnd);
         }
     }
 }
