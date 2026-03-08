f601b288bde05b67b13a2f0ce873fb5cd2942e59
anton stolyarov <a.stolyarov@g.nsu.ru>
2026-03-08 21:47:16 +0700

    combination distribution algorithm is working

--- ris/CrackHash/worker-service/src/main/java/org/crackhash/services/TaskCompletionService.java
+++ ris/CrackHash/worker-service/src/main/java/org/crackhash/services/TaskCompletionService.java
@@ -20,6 +20,8 @@ public class TaskCompletionService {
     private final RestClient restClient;
     private final Range range;
 
+    //TODO: clean up
+
     @Async
     public void complete(CrackHashManagerRequest dto) {
         String hash = dto.getHash();
@@ -31,31 +33,54 @@ public class TaskCompletionService {
 
         long rangeCount = range.start;
         log.info("starting length: {}", range.startingCombinationLength);
-        log.info("ending length: {}", range.endingHashLength);
+        log.info("ending length: {}", range.endingCombinationLength);
         log.info("start: {}", range.start);
         log.info("end: {}", range.end);
-        for (int hashLength = range.startingCombinationLength; hashLength <= range.endingHashLength; hashLength++) {
-            log.info("hash length: {}", hashLength);
-            Iterable<List<String>> generator = Generator.permutation(alphabet)
-                    .withRepetitions(hashLength);
+        for (int combinationLength = range.startingCombinationLength; combinationLength <= range.endingCombinationLength; combinationLength++) {
+            log.info("hash length: {}", combinationLength);
+            Iterable<List<String>> generator = Generator.permutation(alphabet).withRepetitions(combinationLength);
 
             long i = 0;
             for (List<String> combination : generator) {
-                //TODO: introduce 2-element array and checks for starting and ending combination length
-                if (range.generatorIterationStart <= i && i <= range.generatorIterationEnd) {
-                    //                log.info("range count: {}", rangeCount);
-                    if (rangeCount > range.end) {
-                        break;
+                if (range.startingCombinationLength != range.endingCombinationLength) {
+                    if (
+                            (range.generatorIterationStart <= i && combinationLength == range.startingCombinationLength)
+                                    || (i <= range.generatorIterationEnd && combinationLength == range.endingCombinationLength)
+                    ) {
+                        log.info("range count: {}", rangeCount);
+                        if (rangeCount > range.end) {
+                            break;
+                        }
+                        rangeCount++;
+
+                        String candidate = String.join("", combination);
+                        log.info("Processing word: {}", candidate);
+
+                        if (DigestUtils.md5Hex(candidate).equals(hash)) {
+                            log.info("Range: {}", rangeCount);
+                            log.info("Found word with a matching hash: {}; rangeCount: {}", candidate, rangeCount);
+                            return;
+                        }
                     }
-                    rangeCount++;
+                } else {
+                    if (
+                            (range.generatorIterationStart <= i && combinationLength == range.startingCombinationLength)
+                                    && (i <= range.generatorIterationEnd && combinationLength == range.endingCombinationLength)
+                    ) {
+                        log.info("range count: {}", rangeCount);
+                        if (rangeCount > range.end) {
+                            break;
+                        }
+                        rangeCount++;
 
-                    String candidate = String.join("", combination);
-//                log.info("Processing word: {}", candidate);
+                        String candidate = String.join("", combination);
+                        log.info("Processing word: {}", candidate);
 
-                    if (DigestUtils.md5Hex(candidate).equals(hash)) {
-                        log.info("Range: {}", rangeCount);
-                        log.info("Found word with a matching hash: {}", candidate);
-                        return;
+                        if (DigestUtils.md5Hex(candidate).equals(hash)) {
+                            log.info("Range: {}", rangeCount);
+                            log.info("Found word with a matching hash: {}; rangeCount: {}", candidate, rangeCount);
+                            return;
+                        }
                     }
                 }
                 ++i;
@@ -92,7 +117,7 @@ public class TaskCompletionService {
          * Needed to determine on which combination length to end.
          *
          */
-        private int endingHashLength;
+        private int endingCombinationLength;
         /**
          * Starting point for iterations in a generator with a certain hash length.
          * Needed to determine, which combinations the current worker will be checking,
@@ -134,7 +159,6 @@ public class TaskCompletionService {
                     log.info("start: {}", start);
                     if (t >= start) {
                         startingCombinationLength = hashLength;
-                        generatorIterationStart = start - 1;
                         break;
                     }
                 }
@@ -143,14 +167,14 @@ public class TaskCompletionService {
 
         private void calculateEndingLength(int hashMaxLength, int alphabetSize) {
             long t = 0;
-            endingHashLength = 0;
+            endingCombinationLength = 0;
             for (int hashLength = 1; hashLength <= hashMaxLength; hashLength++) {
                 t += (long) Math.pow(alphabetSize, hashLength);
                 log.info("HAHHHHHAHAHHAHH");
                 log.info("t: {}", t);
                 log.info("end: {}", end);
                 if (t >= end) {
-                    endingHashLength = Math.max(hashLength, 1);
+                    endingCombinationLength = Math.max(hashLength, 1);
                     break;
                 }
             }
@@ -158,11 +182,17 @@ public class TaskCompletionService {
 
         private void calculateIterationBounds(int alphabetSize) {
             long t = 0;
-            for (int combinationLength = 1; combinationLength <= startingCombinationLength - 1; combinationLength++) {
-                t += (long) Math.pow(alphabetSize, combinationLength);
+            for (int i = 1; i <= startingCombinationLength - 1; i++) {
+                t += (long) Math.pow(alphabetSize, i);
             }
-            generatorIterationStart = Math.max(start - (long) Math.pow(alphabetSize, startingCombinationLength - 1), 0);
-            generatorIterationEnd = end - (long) Math.pow(alphabetSize, endingHashLength - 1);
+            generatorIterationStart = Math.max(start - t, 0);
+            t = 0;
+
+            for (int i = 1; i <= endingCombinationLength - 1; i++) {
+                t += (long) Math.pow(alphabetSize, i);
+                log.info("CHEKING T VALUE: {}", t);
+            }
+            generatorIterationEnd = end - t;
             log.info("Iteration start: {}", generatorIterationStart);
             log.info("Iteration end: {}", generatorIterationEnd);
         }
