703efe1aed38c1075e3f1bc8d6f83ae9adf7bb40
anton stolyarov <a.stolyarov@g.nsu.ru>
2026-03-11 16:43:17 +0700

    refactored compose + added env vars

--- ris/CrackHash/manager-service/src/main/java/org/crackhash/service/CrackTaskCreationService.java
+++ ris/CrackHash/manager-service/src/main/java/org/crackhash/service/CrackTaskCreationService.java
@@ -1,11 +1,21 @@
 package org.crackhash.service;
 
+import lombok.AllArgsConstructor;
+import org.crackhash.dto.request.CrackTaskCreationRequest;
 import org.crackhash.dto.response.CrackTaskCreationResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@AllArgsConstructor
 public class CrackTaskCreationService {
+    private final CrackTaskStatusTrackerService crackTaskStatusTrackerService;
+
     public CrackTaskCreationResponse handleCrackRequest() {
-        return new CrackTaskCreationResponse();
+        String requestId = UUID.randomUUID().toString();
+        CrackTaskCreationResponse request = new CrackTaskCreationResponse();
+        request.setRequestId(requestId);
+        return request;
     }
 }
